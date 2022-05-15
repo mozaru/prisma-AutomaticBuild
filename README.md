4ddf97963480d65dedd7bb7093a8393c53790986
mozaru <mozar.silva@gmail.com>
2022-05-15 12:10:18 -0300

    first version

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# prisma-AutomaticBuild
+# Build automatico do prisma
+
+## abstract
 tool for automatic code build on the prism development platform It integrates with the platform website, located at http://www.prisma.dev.br/
+
+
+## requisitos
+precisa estar cadastrado e ativado na plataforma do prisma
+[prisma.dev.br](https://prism-dev-platform.herokuapp.com/)
+
+## precisa configurar as variaveis de ambiente
+
+* PRISM_DEV_PLAT_EMAIL         - com o email cadastrado
+* PRISM_DEV_PLAT_PASSWORD      - com a senha do usuario
+* PRISM_DEV_PLAT_INPUT_PATH    - diretorio local onde estao os arquivos mzdl e o arquivo config.json
+
+## ex no windows:
+  set PRISM_DEV_PLAT_EMAIL=meuemail@teste.com
+  set PRISM_DEV_PLAT_PASSWORD=minhasenha
+  set PRISM_DEV_PLAT_INPUT_PATH=c:\projetos\teste_prima\src_mz
+
+## executar 
+  node index.js
+
+# Arquivo de configuracao modelo
+
+este arquivo deve se chamar "config.json" e deve estar na raiz do projeto mzdl
+veja um exemplo de projeto (aqui)
+
+
+~~~
+{
+    "project":"NOME DO PROJETO ex:Agenda",
+    "version":"1.0",
+    "author":"Mozar Baptista da Silva",
+    "outputRootPath":"../src",
+    "inputGit":"",
+    "outputGit":"",
+    "profile":"fullstack:html5.0-dotnet5.0-sqlite3.0:1.0",
+    "constants":{
+        "DATA_BASE_TECHONOLOGY":"SQLITE",
+        "NAME_SPACE":"NAMESPACE DO PROJETO ex:Agenda",
+        "BACKEND_NAME":"NOME DO PROJETO ex:agenda",
+        "GUID_PROJ":"auto",
+        "GUID_SOL":"auto",
+        "GUID_CFG":"auto",
+        "GUID_BUILD":"auto",
+        "DB_HOST":"",
+        "DB_PORT":"",
+        "DB_DATABASE":"dados",
+        "DB_LOGIN":"",
+        "DB_PASSWORD":"",
+        "EMAIL_HOST":"smtp.gmail.com",
+        "EMAIL_PORT":"587",
+        "EMAIL_PROTOCOL":"ssl",
+        "EMAIL_USER":"seuemail@gmail.com",
+        "EMAIL_PASSWORD":"suasenha",
+        "SERVER_HOST":"",
+        "OAUTH_CHAVE":"TESTE DE CHAVE DE ACESSO 2022051",
+        "OAUTH_CHAVES_VALIDADE":"2",
+        "OAUTH_ACCESS_TOKEN_VALIDADE":"40",
+        "OAUTH_REFRESH_TOKEN_VALIDADE":"8",
+        "OAUTH_TIPO_TOKEN_ACESSO":"Acesso",
+        "OAUTH_TIPO_TOKEN_REFRESH":"Refresh",
+        "OAUTH_TIPO_TOKEN_DEFAULT":"_OAUTH_TIPO_TOKEN_ACESSO_",
+        "OAUTH_PERFIL_PADRAO":"Admin",
+        "AD_PATH":"",
+        "AD_USUARIO":"",
+        "AD_SENHA":""        
+    }
+}
+
+
+~~~
---
+++ index.js
@@ -0,0 +1,275 @@
+const https = require('https'); // or 'https' for https:// URLs
+const fs = require('fs');
+const request = require('request');
+const path = require('path');
+const AdmZip = require('adm-zip');
+const uuid = require('uuid');
+const process = require('process');
+
+const URL_BASE = 'https://prism-dev-platform.herokuapp.com/';
+//const URL_BASE = 'https://localhost:1080/';
+
+var USER_NAME = process.env.PRISM_DEV_PLAT_EMAIL;
+var PASSWORD = process.env.PRISM_DEV_PLAT_PASSWORD;
+var INPUT_PATH = process.env.PRISM_DEV_PLAT_INPUT_PATH;
+
+const TEMP_FILE = process.env.PRISM_DEV_PLAT_TEMP_PATH?path.join(process.env.PRISM_DEV_PLAT_TEMP_PATH,'temp.zip'):'temp.zip';
+
+console.log(process.env);
+
+if (!USER_NAME || !PASSWORD || !INPUT_PATH) { 
+    console.error("Falha nos parametros!");
+
+    console.log("é obrigatório configurar as variaveis de ambiente:");
+    console.log('PRISM_DEV_PLAT_EMAIL');
+    console.log('PRISM_DEV_PLAT_PASSWORD');
+    console.log('PRISM_DEV_PLAT_INPUT_PATH');
+
+    console.log('\nOpcionalmente voce pode configurar o diretorio temporario: PRISM_DEV_PLAT_TEMP_PATH');
+    return;
+}
+
+var access_token = "";
+
+async function downloadFile(url, outputFileName)
+{
+    let file = fs.createWriteStream(outputFileName);
+    await new Promise((resolve, reject) => {
+        let stream = request({
+        uri: url,
+        headers: {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8,ro;q=0.7,ru;q=0.6,la;q=0.5,pt;q=0.4,de;q=0.3',
+            'Cache-Control': 'max-age=0',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+        },
+        gzip: true
+        })
+        .pipe(file)
+        .on('finish', () => {
+            console.log(`The file is finished downloading.`);
+            resolve();
+        })
+        .on('error', (error) => {
+            reject(error);
+        })
+    })
+    .catch(error => {
+        console.log(`Something happened: ${error}`);
+    });
+}
+
+
+function getAllVariables(txt)
+{
+    const regexp = new RegExp('#\\w+#','g');
+    let variables = [];
+    for(const match of txt.matchAll(regexp))
+    {
+        const name = match[0].trim();
+        if (!variables.includes(name))
+            variables.push(name);
+    }      
+    return variables;
+}
+
+function extractFiles(outputdir, zipFileName, variables){
+    var zip = new AdmZip(zipFileName);
+    var zipEntries = zip.getEntries(); // an array of ZipEntry records
+    const regexp = RegExp('#\w+#','g');
+    for(zipEntry of zipEntries)
+    {
+        const fullName = path.join(outputdir,zipEntry.entryName);
+        if (zipEntry.isDirectory)
+        {
+            console.log('filename:',fullName);
+    
+            if (!fs.existsSync(fullName))
+            {
+                console.log('create dir:',fullName);
+                fs.mkdirSync(fullName,{recursive: true});
+            }    
+        }
+        else if (zipEntry.name!='config.vars')
+        {
+            let str = zipEntry.getData().toString("utf8");
+            let found = false;
+            for(const name of getAllVariables(str))
+            {
+                const variable = name.replace(new RegExp('#','g'),"").trim();
+                if (variable in variables)
+                {
+                    found = true;
+                    str = str.replace(new RegExp(name,'g'),variables[variable]);
+                }
+            }
+            if (found)
+                fs.writeFileSync(fullName, str);
+            else
+                fs.writeFileSync(fullName,zipEntry.getData());
+        }
+    }
+}
+
+
+async function generateLayer(outputDir, variables, url) {
+    const zipFileName=TEMP_FILE;
+    console.log("download boilerplat from ",url);
+    await downloadFile(url, zipFileName);
+    console.log("extract files to ", outputDir);
+    extractFiles(outputDir, zipFileName, variables);
+    fs.unlinkSync(zipFileName);
+}
+
+/*function getProfile(profile){
+    const profiles = JSON.parse(fs.readFileSync("automatic-build.json",'utf-8'));
+    for(const p of profiles)
+        if (p.profile == profile)
+            return p;
+    return null;
+}*/
+
+async function buildProject(inputDir){
+    const original = process.cwd();
+    process.chdir(inputDir);
+    const config = JSON.parse(fs.readFileSync(path.join(inputDir,"config.json"),'utf-8'));
+    const profile = await getProfile(config.profile);
+    for(const name in config.constants){
+        if (name.toLowerCase().startsWith('guid') && config.constants[name]=='auto')
+            config.constants[name] = uuid.v4();
+    }
+    const outputPath = path.resolve(config.outputRootPath);
+    console.log("outputPath:",outputPath);
+    process.chdir(original);
+    for(const l of profile.layers)
+    {
+        console.log("processing layer ",l.name);
+        const baseOutputPath = path.join(outputPath, l.outputBasePath);
+        if (l.boilerplate && !fs.existsSync(baseOutputPath))
+        {
+            console.log("create layer ",l.name);
+            if (!fs.existsSync(baseOutputPath))
+                fs.mkdirSync(baseOutputPath,{ recursive: true });
+            await generateLayer(baseOutputPath, config.constants, l.boilerplate)
+        }
+    }
+    let creditos=0;
+    for(const l of profile.layers)
+    {
+        console.log("transpile layer ",l.name);
+        for(const t of l.transpilers){
+            console.log("transpile files with: ",t.transpiler);
+            for(const file of fs.readdirSync(path.join(inputDir,t.inputPath)))
+                if (file.endsWith('.mzdl'))
+                {
+                    console.log("transpile file: ",file);
+                    const content = fs.readFileSync(path.join(inputDir,t.inputPath,file), 'utf-8');
+                    const r = await Transpiler(content, t.transpiler, file);
+                    if (r.message)
+                    {
+                        console.error(r.message);
+                        return;
+                    }
+                    creditos = r.creditos;
+                    for(const key in r.files)
+                    {
+                        const targetName = key===''?(t.outputFile?t.outputFile:path.basename(file, path.extname(file))+"."+r.defaultExtension):key;
+                        const baseOutputPath = (t.outputPath===''|| !t.outputPath)?path.join(outputPath,l.outputBasePath):path.join(outputPath,l.outputBasePath, t.outputPath);
+                        if (!fs.existsSync(baseOutputPath))
+                            fs.mkdirSync(baseOutputPath,{ recursive: true });   
+                        fs.writeFileSync(path.join(baseOutputPath,targetName),r.files[key],'utf-8');
+                        console.log("generate file: ",path.join(baseOutputPath,targetName));
+                    }
+                }
+        }
+    }
+    console.log("Creditos: ",creditos);    
+}
+
+
+async function callApiPost(url, body){
+    return await new Promise((resolve, reject) => {
+        let stream = request.post({ url:url, json:body, rejectUnauthorized: false, requestCert: true, headers: {Authorization: access_token?`Bearer ${access_token}`:''} }, 
+            (error, res, body) => {
+            if (error) reject(error);
+            resolve(body);
+        });
+    })
+    .catch(error => {
+        console.log(`Something happened: ${error}`);
+    });
+}
+async function callApiGet(url){
+    return await new Promise((resolve, reject) => {
+        let stream = request.get({ url:url, rejectUnauthorized: false, requestCert: true, headers: {Authorization: access_token?`Bearer ${access_token}`:''} }, 
+            (error, res, body) => {
+            if (error) reject(error);
+            resolve(body);
+        });
+    })
+    .catch(error => {
+        console.log(`Something happened: ${error}`);
+    });
+}
+
+async function login(login, password) {
+    console.log("logando:",login);
+    const body = await callApiPost(URL_BASE+'api/login', { "Login": login, "Password":password });
+    if (body && body.access_token)
+    {
+        console.log("logou!");
+        access_token = body.access_token;
+    } else {
+        console.log("nao logou!");
+        access_token = "";
+    }
+    /*await new Promise((resolve, reject) => {
+        let stream = request.post(URL_BASE+'api/login', { json:{ "Login": login, "Password":password },
+        rejectUnauthorized: false,
+        requestCert: true        
+        }, (error, res, body) => {
+            if (error) {
+              console.error(error);
+              access_token="";
+              reject(error);
+            }
+            console.log(`statusCode: ${res.statusCode}`)
+            console.log(body);
+            access_token = body.access_token;
+            resolve();
+        });
+    })
+    .catch(error => {
+        console.log(`Something happened: ${error}`);
+    });*/
+}
+
+async function getProfile(profile){
+    if (!access_token)
+        await login(USER_NAME,PASSWORD);
+    console.log('Baixando profile:',profile);
+    profile = encodeURIComponent(profile);
+    let resp = await callApiGet(`${URL_BASE}api/profile/ByTechnicalName?TechnicalName=${profile}`);
+    resp = JSON.parse(resp.replace(/\\n|\\t/g,''));
+    console.log('Profile baixado');
+    return JSON.parse(resp.config);
+}
+
+async function Transpiler(source, technicalName, fileName){
+    source = Buffer.from(source, 'utf-8').toString('base64');
+    if (!access_token)
+        await login(USER_NAME,PASSWORD);
+    let resp = await callApiPost(`${URL_BASE}api/transpile`,{"codeBase64":source, "TechnicalName": technicalName, "InputFileName":fileName});
+    if (resp && resp.files)
+        for(const key in resp.files)
+            if (resp.files[key]==='')
+                delete resp.files[key];
+            else
+                resp.files[key] = Buffer.from(resp.files[key], 'base64').toString('utf-8');
+    return resp;
+}
+
+buildProject(INPUT_PATH);
