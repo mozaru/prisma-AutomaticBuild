5dad615342ba89c0edd3c86892ccc914f21b9a6e
mozaru <mozar.silva@gmail.com>
2022-12-28 07:24:23 -0300

    pre-analise e parametros -h,-v,-r,-b,-i,-c,-n,-y

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ const AdmZip = require('adm-zip');
 const uuid = require('uuid');
 const process = require('process');
 const prettier = require("prettier");
+const { isMainThread } = require('worker_threads');
+const { Console } = require('console');
+const chalk=require("chalk");
 
-const URL_BASE = 'https://prism-dev-platform.herokuapp.com/';
+const URL_BASE = 'https://www.prisma.dev.br/';
 //const URL_BASE = 'https://localhost:1080/';
 
 var USER_NAME = process.env.PRISM_DEV_PLAT_EMAIL;
 var PASSWORD = process.env.PRISM_DEV_PLAT_PASSWORD;
 var INPUT_PATH = process.env.PRISM_DEV_PLAT_INPUT_PATH;
+var BUILD_ALL = false;
 
 const TEMP_FILE = process.env.PRISM_DEV_PLAT_TEMP_PATH?path.join(process.env.PRISM_DEV_PLAT_TEMP_PATH,'temp.zip'):'temp.zip';
 
@@ -29,6 +33,7 @@ if (!USER_NAME || !PASSWORD || !INPUT_PATH) {
 }
 
 var access_token = "";
+var creditos = -1;
 
 async function downloadFile(url, outputFileName)
 {
@@ -154,6 +159,74 @@ function includeExternalFiles(content, inputDir)
     return content;
 }
 
+function countLines(content){
+    return content.split('\n').reduce( (count,line) => { return line.trim()?count+1:count; }, 0);
+}
+
+function readBuildFile(fullpath){
+    if (BUILD_ALL)
+        return {};
+    var dictionary = {};
+    if (fs.existsSync(fullpath)){
+        const items = JSON.parse(fs.readFileSync(fullpath,'utf-8'));
+        for (var i = 0; i < items.length; i++) {
+            var item = items[i];
+            dictionary[item.transpiler+":"+item.path] = item;
+        }
+    }
+
+    return dictionary;
+}
+function writeBuildFile(fullpath, dic){
+    var vet=[];
+    for(const key of Object.keys(dic))
+        vet.push(dic[key]);
+    fs.writeFileSync(fullpath,JSON.stringify(vet),'utf-8');
+}
+
+async function analiseBuild(inputDir){
+    const original = process.cwd();
+    console.log("Analisando arquivos modificados");
+    process.chdir(inputDir);
+    const config = JSON.parse(fs.readFileSync(path.join(inputDir,"config.json"),'utf-8'));
+    const profile = await getProfile(config.profile);
+    process.chdir(original);
+    let response = [];
+    const dict = readBuildFile(path.join(inputDir,".mzdlbuild"))
+    for(const l of profile.layers)
+    {
+        console.log("analisando layer ",l.name);
+        for(const t of l.transpilers)
+        {
+            for(const file of fs.readdirSync(path.join(inputDir,t.inputPath)))
+            {
+                if (file.endsWith('.mzdl'))
+                {
+                    const fullPathFile = path.join(inputDir,t.inputPath,file);
+                    const relativePathFile = fullPathFile.substring(inputDir.length)
+                    const key = t.transpiler+":"+relativePathFile; 
+                    const lastModify = fs.statSync(fullPathFile).mtimeMs;
+                    if (!dict[key] || lastModify != dict[key].lastModify)
+                    {
+                        let fileMZDL = response.find( x => x.fullPath==relativePathFile );
+                        fileMZDL = fileMZDL? fileMZDL : { fullPath:relativePathFile, lines:0, transpilations:0, transpilers: []};
+                        if (fileMZDL.transpilations==0)
+                        {
+                            let content = fs.readFileSync(path.join(inputDir,t.inputPath,file), 'utf-8');
+                            content = includeExternalFiles(content, path.join(inputDir,t.inputPath));
+                            fileMZDL.lines = countLines(content);
+                            response.push(fileMZDL);
+                        }
+                        fileMZDL.transpilations++;
+                        fileMZDL.transpilers.push(t.transpiler);
+                    }
+                }
+            }
+        }
+    }
+    return response;
+}
+
 async function buildProject(inputDir){
     const original = process.cwd();
     process.chdir(inputDir);
@@ -180,6 +253,7 @@ async function buildProject(inputDir){
     }
     let creditos=0;
     let variaveis = {};
+    let dict = readBuildFile(path.join(inputDir,".mzdlbuild"));
     for(const l of profile.layers)
     {
         console.log("transpile layer ",l.name);
@@ -188,36 +262,46 @@ async function buildProject(inputDir){
             for(const file of fs.readdirSync(path.join(inputDir,t.inputPath)))
                 if (file.endsWith('.mzdl'))
                 {
-                    console.log("transpile file: ",file);
-                    let content = fs.readFileSync(path.join(inputDir,t.inputPath,file), 'utf-8');
-                    content = includeExternalFiles(content, path.join(inputDir,t.inputPath));
-                    const r = await Transpiler(content, t.transpiler, file, variaveis);
-                    if (r.message)
+                    const fullPathFile = path.join(inputDir,t.inputPath,file);
+                    const relativePathFile = fullPathFile.substring(inputDir.length)
+                    const key = t.transpiler+":"+relativePathFile; 
+                    const lastModify = fs.statSync(fullPathFile).mtimeMs;
+                    if (!dict[key] || lastModify != dict[key].lastModify)
                     {
-                        console.error(r.message);
-                        return;
-                    }
-                    variaveis = r.variaveis;
-                    creditos = r.creditos;
-                    for(const key in r.files)
-                    {
-                        const targetName = key===''?(t.outputFile?t.outputFile:path.basename(file, path.extname(file))+"."+r.defaultExtension):key;
-                        const baseOutputPath = (t.outputPath===''|| !t.outputPath)?path.join(outputPath,l.outputBasePath):path.join(outputPath,l.outputBasePath, t.outputPath);
-                        if (!fs.existsSync(baseOutputPath))
-                            fs.mkdirSync(baseOutputPath,{ recursive: true });
-						const fullPathFile = path.join(baseOutputPath,targetName);
-						let fileContent = r.files[key];
-						fs.writeFileSync(fullPathFile,fileContent,'utf-8');
-						try{
-						    const prettierConfig = prettier.resolveConfig.sync(fullPathFile);
-						    fileContent = prettier.format(fileContent, {endOfLine: 'auto', tabWidth: 4, embeddedLanguageFormatting:'auto', filepath: fullPathFile});
+                        dict[key] = { transpiler: t.transpiler, path: relativePathFile, lastModify:lastModify};
+                        console.log("transpile file: ",file);
+                        let content = fs.readFileSync(path.join(inputDir,t.inputPath,file), 'utf-8');
+                        content = includeExternalFiles(content, path.join(inputDir,t.inputPath));
+                        const r = await Transpiler(content, t.transpiler, file, variaveis);
+                        if (r.message)
+                        {
+                            console.error(r.message);
+                            return;
+                        }
+                        variaveis = r.variaveis;
+                        creditos = r.creditos;
+                        
+                        for(const key in r.files)
+                        {
+                            const targetName = key===''?(t.outputFile?t.outputFile:path.basename(file, path.extname(file))+"."+r.defaultExtension):key;
+                            const baseOutputPath = (t.outputPath===''|| !t.outputPath)?path.join(outputPath,l.outputBasePath):path.join(outputPath,l.outputBasePath, t.outputPath);
+                            if (!fs.existsSync(baseOutputPath))
+                                fs.mkdirSync(baseOutputPath,{ recursive: true });
+                            const fullPathFile = path.join(baseOutputPath,targetName);
+                            let fileContent = r.files[key];
                             fs.writeFileSync(fullPathFile,fileContent,'utf-8');
-						}catch(err){}
-                        console.log("generate file: ",path.join(baseOutputPath,targetName));
+                            try{
+                                const prettierConfig = prettier.resolveConfig.sync(fullPathFile);
+                                fileContent = prettier.format(fileContent, {endOfLine: 'auto', tabWidth: 4, embeddedLanguageFormatting:'auto', filepath: fullPathFile});
+                                fs.writeFileSync(fullPathFile,fileContent,'utf-8');
+                            }catch(err){}
+                            console.log("generate file: ",path.join(baseOutputPath,targetName));
+                        }
                     }
                 }
         }
     }
+    writeBuildFile(path.join(inputDir,".mzdlbuild"),dict);
     console.log("Creditos: ",creditos);    
 }
 
@@ -250,6 +334,7 @@ async function callApiGet(url){
 async function login(login, password) {
     console.log("logando:",login);
     const body = await callApiPost(URL_BASE+'api/login', { "Login": login, "Password":password });
+    creditos = body.creditos;
     if (body && body.access_token)
     {
         console.log("logou!");
@@ -304,4 +389,130 @@ async function Transpiler(source, technicalName, fileName, variaveis){
     return resp;
 }
 
-buildProject(INPUT_PATH);
+var readlineSync = require('readline-sync');
+const { CENFLG } = require('adm-zip/util/constants');
+const { compileFunction } = require('vm');
+
+function mostrarConsumo(analise) {
+    let linhas = 0;
+    let linhastranspiladas = 0;
+    let transpilacoes = 0;
+    let files = 0;
+    for(const x of analise) {
+        linhas += x.lines;
+        transpilacoes += x.transpilations;
+        linhastranspiladas += x.lines*x.transpilations;
+        files++;
+    }
+    const creditosConsumidos = transpilacoes;
+    const saldo = creditos - creditosConsumidos;
+    console.log(chalk.yellow('-----------------------------------------'));
+    console.log(`Arquivos Analisados: ${files}`);
+    console.log(`Linhas Analisadas: ${linhas}`);
+    console.log(`Transpilacoes: ${transpilacoes}`);
+    console.log(`Linhas para Transpilar: ${linhastranspiladas}`);
+    console.log(`Creditos a serem consumidos: ${chalk.red(creditosConsumidos)}`);
+    console.log(`Creditos atual: ${chalk.green(creditos)}`);
+    let strSaldo;
+    if (saldo>0)
+        strSaldo = chalk.green(saldo);
+    else if (saldo==0)
+        strSaldo = chalk.yellow(saldo);
+    else
+        strSaldo = chalk.red(saldo);
+    console.log(`Creditos saldo: ${strSaldo}`);
+    console.log(chalk.yellow('-----------------------------------------'));
+    return saldo;
+}
+
+function processarParametros() {
+    cfg = { ajuda:false, build:true, analise:false, clear:false, buildAll:false, inputDir:null, responseAll:null, error:false }
+    
+    for(const x of process.argv.filter((val, index) => {return index>=2} ))
+    {
+        const key = (x.indexOf('=')>-1?x.substring(0,x.indexOf('=')):x).toLowerCase();
+        switch(key)
+        {
+            case '-h': cfg.ajuda = true; cfg.build = false;break;
+            case '-v': cfg.analise = true; cfg.build = false;break;
+            case '-b': cfg.build = true; break;
+            case '-c': cfg.clear = true; cfg.build = false;break;
+            case '-r': cfg.buildAll = true; break;
+            case '-y': cfg.responseAll = 'y'; break;
+            case '-n': cfg.responseAll = 'n'; break;
+            case '-i': cfg.inputDir = x.substring(x.indexOf('=')+1).trim(); break;
+            default  : console.log("chave invalida de configuracao: "+x);
+                       cfg.error=true; 
+                       break;
+        }
+    }
+    return cfg;
+}
+
+async function main()
+{
+    var config = processarParametros();
+    if (config.ajuda || config.error)
+    {
+        console.log("node start -- <params>");
+        console.log("-h                esta ajuda");
+        console.log("-v                analise do codigo");
+        console.log("-b                build do codigo");
+        console.log("-c                limpar historico");
+        console.log("-r                recompilar tudo");
+        console.log("-y                sempre responder a sim/yes");
+        console.log("-n                sempre responder a nao/no");
+        console.log("-i=<inputdir>     diretorio do projeto a ser transpilado");
+        console.log('\n\n');
+        return;
+    }
+    if (config.inputDir)
+        INPUT_PATH = config.inputDir;
+
+    //gambi para remover excessos de "path.sep"    
+    INPUT_PATH=path.join(INPUT_PATH);
+    if (!INPUT_PATH.endsWith(path.sep))
+        INPUT_PATH = INPUT_PATH+path.sep;
+
+
+    if (config.clear)
+    {
+        const file = path.join(INPUT_PATH,".mzdlbuild");
+        if (fs.existsSync(file))
+            fs.rmSync(file);
+        return;
+    }
+
+    if (config.buildAll)
+        BUILD_ALL = true;
+
+    const r = await analiseBuild(INPUT_PATH);
+    if (r.length==0){
+        console.log("Nenhum arquivo foi modificado!");
+        return;
+    }
+    const saldo = mostrarConsumo(r);
+    if (saldo<0)
+    {
+        console.log("Creditos insuficientes para fazer a transpilacao!");
+        console.log("Para transpilar adquira mais creditos em: "+chalk.yellow('https:\\\\www.prisma.dev.br\\creditos'));
+        return;
+    }
+    
+    if (config.build)
+    {
+        let resp;
+        if (config.responseAll==null)
+            do{
+                resp = readlineSync.question("Deseja realizar as transpilacoes (S/N)? ").toLowerCase();
+            }while(resp!="s" && resp!='n' && resp !='y');
+        else
+            resp = config.responseAll.toLowerCase();
+        if (resp=="s" || resp=="y")
+            await buildProject(INPUT_PATH);
+    }
+}
+
+
+
+main();
